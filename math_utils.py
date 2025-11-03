827b2a78f9c9ef4f1cc80cff89077efa710a2ab8
monehad06 <mo.nehad06@gmail.com>
2025-11-04 00:20:23 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -1,2 +1,8 @@
 def find_max_number(num1, num2, num3):
-    pass  # Replace 'pass' with code
+    def find_max_number(num1, num2, num3):
+    if num1 > num2 and num1 > num3:
+        return num1
+    elif num2 > num3:
+        return num2
+    else:
+        return num3
